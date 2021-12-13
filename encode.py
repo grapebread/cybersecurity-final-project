5325e78ef9f7af7c0ca9578f6f51bc8d6080ee6b
Josephine Lee <lee.josie.y@gmail.com>
2021-12-13 15:13:00 -0500

    clean up code

--- encode.py
+++ encode.py
@@ -8,23 +8,23 @@ input = sys.argv[2]
 input = input.lower()
 mode = sys.argv[3]
 mode = mode.lower()
-l1 = tonum(key[0]) + 1
-l2 = tonum(key[1]) + 1
-l3 = tonum(key[2]) + 1
-l4 = tonum(key[3]) + 1
-key = np.array([[l1, l2],[l3, l4]])
-key_rows = key.shape[0]
+part1 = tonum(key[0]) + 1
+part2 = tonum(key[1]) + 1
+part3 = tonum(key[2]) + 1
+part4 = tonum(key[3]) + 1
+key = np.array([[part1, part2],[part3, part4]])
+KEYrows = key.shape[0]
 
-def encrypt(message, key): 
-    encryption = key @ message 
+def encrypt(messageToEncode, key): 
+    encryption = key @ messageToEncode 
     encryption = np.remainder(encryption, 26) 
     return encryption
 
 def encode():
-    message = tomatrix(input, key_rows) 
+    messageToEncode = tomatrix(input, KEYrows) 
     encryptm = "" 
-    for i in range(0, len(message)):
-            encrypted = encrypt(message[i], key) 
+    for i in range(0, len(messageToEncode)):
+            encrypted = encrypt(messageToEncode[i], key) 
             encryptm = encryptm + toletter(encrypted[0]) + toletter(encrypted[1]) 
     return encryptm
 

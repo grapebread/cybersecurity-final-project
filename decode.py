b3c886499d0e21826c805d66280273b79df8fe33
shushu <golpos369@gmail.com>
2021-12-13 08:16:54 -0500

    added conversion function

--- decode.py
+++ decode.py
@@ -3,6 +3,9 @@ import numpy as np
 
 # Note this is only for the 2x2 case
 
+ciphertext = sys.argv[1].lower()
+key = sys.argv[2].lower()
+
 """
 returns the inverse of the matrix parameter in mod 26
 matrix represents some invertible matrix (square numpy array with a non-zero determinant)
@@ -21,6 +24,18 @@ def inv(matrix):
 
     return adj
 
+"""
+if n is an int, return corresponding char
+if n is a char, return corresponding int
+"""
+def convert(n):
+    if type(n) == int:
+        return chr(n + 97)
+    elif type(n) == str:
+        return ord(n.lower()) - 97
+
+for i in "HELP":
+    print(convert(i))
 
 """
 cipher_matrix is a n-component vector (i.e [1, 2, 3, 4, 5])
@@ -38,6 +53,14 @@ def decode(cipher_matrix, key):
         
         while (len(chars) < len(key)): chars.append([0])
 
-        plain_matrix.append(np.array(chars))
+        print(chars)
+        dot = inv(key) @ chars
+
+        for n in dot:
+            plain_matrix.append(n)
 
     return plain_matrix
+
+cipher = np.array([[7, 8], [0, 19]])
+key = np.array([[3, 3], [2, 5]])
+#print(decode(cipher, key))

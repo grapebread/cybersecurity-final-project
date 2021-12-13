0f53ee37eb41479f872c4bfacceb972dae12be9a
Josephine Lee <lee.josie.y@gmail.com>
2021-12-13 15:02:23 -0500

    fix shape error

--- encode.py
+++ encode.py
@@ -8,6 +8,11 @@ input = sys.argv[2]
 input = input.lower()
 mode = sys.argv[3]
 mode = mode.lower()
+l1 = tonum(key[0]) + 1
+l2 = tonum(key[1]) + 1
+l3 = tonum(key[2]) + 1
+l4 = tonum(key[3]) + 1
+key = np.array([[l1, l2],[l3, l4]])
 key_rows = key.shape[0]
 
 def encrypt(message, key): 
